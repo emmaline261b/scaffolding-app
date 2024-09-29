d05c2700126d87fad2688eaf41b0d7c141541f6b
Emmaline261 <emmaline261@gmail.com>
2024-09-29 20:12:51 +0200

    ready structure with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {MainComponent} from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: MainComponent },
+  { path: '', component: MainComponent }
+];
